326a89a0124935f6d0e2e54b1ea92aa3cf88963c
yeagifleur@gmail.com <yeagifleur@gmail.com>
2024-07-24 08:43:05 +0000

    int_orders_operational

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,9 +1,14 @@
 SELECT 
     om.orders_id
     ,om.date_date
+    ,ROUND(om.margin + sh.shipping_fee - (sh.logcost + sh.ship_cost),2) AS operational_margin
     ,om.revenue
-    ,om.margin    
-    ,om.margin + sh.shipping_fee - (sh.logcost + sh.ship_cost) AS operational_margin
+    ,om.purchase_cost
+    ,om.margin
+    ,sh.shipping_fee
+    ,sh.logcost
+    ,sh.ship_cost
 FROM {{ref("int_orders_margin")}} AS om
 LEFT JOIN {{ref("stg_gz_raw_data__raw_gz_ship")}} AS sh
-USING (orders_id)
+    USING (orders_id) 
+ORDER BY orders_id DESC 
